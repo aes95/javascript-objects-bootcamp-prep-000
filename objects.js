147c92ed74c2bbb2c6520cb505b3e3dba73068e7
Aviad Sapir <aviadsapir@gmail.com>
2018-09-14 12:36:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,9 +2,9 @@ const playlist = {breakeven:'the script'};
 
 function updatePlaylist(playlist, artist, song){
   playlist[artist] = song;
-  return playlist
+  return playlist;
 }
 function removeFromPlaylist (playlist, artist){
   delete playlist.artist;
-  return playlist
+  return playlist;
 }
